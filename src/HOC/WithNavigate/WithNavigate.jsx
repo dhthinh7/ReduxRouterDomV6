3b2587f60299d103a0c0be2925d6a03ec55d62f7
dhthinh7 <dhthinh7@gmail.com>
2022-09-29 15:55:47 +0700

    Add React router dom V6

---
+++ src/HOC/WithNavigate/WithNavigate.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export function WithNavigate(Component) {
+
+
+    const navigate = useNavigate();
+    return function (props) {
+
+        return <Component navigate={navigate} {...props} />
+    }
+}
